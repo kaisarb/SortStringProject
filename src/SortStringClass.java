d8984ddf6f7cd620377923a264e33c8869799490
Kaisar <kaisarb@gmail.com>
2022-08-08 12:39:19 +0600

    Initial commit

---
+++ src/SortStringClass.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class SortStringClass {
+    public static void main(String[] args) {
+        String test= "edcba";
+        char[] charArray = test.toCharArray();
+        Arrays.sort(charArray);
+        System.out.println(charArray);
+        System.out.print("adfdag".charAt(2));
+    }
+}
